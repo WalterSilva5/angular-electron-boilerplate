f6163768907e420d3e3f1d4907c0abcb54ad78c7
waltersilva5 <waltersilva5@outlook.com>
2025-02-09 19:05:24 -0300

    Update app-routing.module.ts

--- src/renderer/src/app/app-routing.module.ts
+++ src/renderer/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { SystemInfoComponent } from './pages/system-info/system-info.component';
 
 const routes: Routes = [
+  { path:  '/', component:  AboutComponent },
   { path:  'about', component:  AboutComponent },
   { path:  'info', component:  SystemInfoComponent },
 //  { path: '404', component: NotfoundComponent },
